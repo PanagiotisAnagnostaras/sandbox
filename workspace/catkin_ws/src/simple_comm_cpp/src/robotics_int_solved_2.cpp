263e6cf08659a328973eb5ec3c93b3c3e425fe69
Panagiotis Anagnostaras <panagiotis.anagnostaras@kemaro.ch>
2024-12-14 16:45:46 +0100

    made thread implementation

--- workspace/catkin_ws/src/simple_comm_cpp/src/robotics_int_solved_2.cpp
+++ workspace/catkin_ws/src/simple_comm_cpp/src/robotics_int_solved_2.cpp
@@ -1,47 +1,58 @@
 /*
 Ben wrote this node at 4AM... can you spot his mistakes?
 */
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "std_msgs/Int32MultiArray.h"
-
 #include <sstream>
+#include <thread>
 
-void timerCallback(const ros::TimerEvent&) {
-        std::vector<int> my_array = new std::vector<int>(1000, 0);
-        std_msgs::Int32MultiArray msg;
-        msg.data = *my_array;
-        pub_.publish(msg);
-        ROS_INFO("Published an array of size: %lu", msg.data.size());
+#include "ros/ros.h"
+#include "std_msgs/Int32MultiArray.h"
+#include "std_msgs/String.h"
+
+class DrungPublisher {
+ public:
+  DrungPublisher(ros::NodeHandle nh) : nh_(nh) {
+    chatter_pub_ = nh_.advertise<std_msgs::String>("chatter", 1000);
+    array_pub_ = nh_.advertise<std_msgs::Int32MultiArray>("array", 0);
+    ros::Timer timer = nh_.createTimer(ros::Duration(1.0),
+                                       &DrungPublisher::_timerCallback, this);
+    std::thread thread_main_loop(&DrungPublisher::_main_loop, this);
+    ros::spin();
+  };
+  ~DrungPublisher() {};
+
+ private:
+  void _timerCallback(const ros::TimerEvent& event) {
+    std::vector<int> my_array = std::vector<int>(1000, 0);
+    std_msgs::Int32MultiArray msg;
+    msg.data = my_array;
+    array_pub_.publish(msg);
+    ROS_INFO("Published an array of size: %lu", msg.data.size());
+  };
+
+  void _main_loop() {
+    ros::Rate loop_rate(10);
+    int count = 0;
+    while (ros::ok()) {
+      std_msgs::String msg;
+      std::stringstream ss;
+      ss << "hello world " << count;
+      msg.data = ss.str();
+      ROS_INFO("%s", msg.data.c_str());
+      chatter_pub_.publish(msg);
+      loop_rate.sleep();
+      ++count;
     }
-
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "dummy");
-  ros::NodeHandle n;
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-  ros::Publisher array_pub = n.advertise<std_msgs::UInt8MultiArray>("array", 0);
-  ros::Timer timer = nh.createTimer(ros::Duration(1.0), timerCallback, n);
-
-  ros::Rate loop_rate(10);
-
-  int count = 0;
-  while (ros::ok())
-  {
-    std_msgs::String* msg;
-
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
-
-    ROS_INFO("%s", msg.data.c_str());
-    chatter_pub.publish(msg);
-
-    loop_rate.sleep();
-    ++count;
   }
 
+  ros::NodeHandle nh_;
+  ros::Publisher chatter_pub_;
+  ros::Publisher array_pub_;
+  ros::Timer timer_;
+};
 
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "dummy");
+  ros::NodeHandle n;
+  DrungPublisher drungPublisher(n);
   return 0;
 }
-
